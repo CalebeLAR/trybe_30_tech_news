1bbc161016c9823e70b81aa6d091672b56ffa15d
calebeLAR <ca6ele@gmail.com>
2023-09-12 16:27:09 -0400

    req01: implementa função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,37 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    """
+    Realiza uma requisição HTTP GET para a URL especificada, com um intervalo
+    mínimo de 1 segundo entre as requisições, utilizando um cabeçalho
+    "user-agent" falso para simular um agente de usuário.
+
+    Args:
+        url (str): A URL de destino para a requisição.
+
+    Returns:
+        str or None: O conteúdo da resposta da requisição como texto, caso a
+        requisição seja bem-sucedida, ou None em caso de falha na requisição.
+
+    Raises:
+        requests.RequestException: Exceção lançada se ocorrer um erro na
+        realização da requisição.
+    """
+
+    headers = {"user-agent": "Fake user-agent"}
+
+    try:
+        time.sleep(1)
+        response = requests.get(url=url, headers=headers, timeout=3)
+        response.raise_for_status()
+
+        return response.text
+    except requests.RequestException:
+
+        return None
 
 
 # Requisito 2
