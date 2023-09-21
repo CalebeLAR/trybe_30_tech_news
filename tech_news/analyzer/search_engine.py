d0a2ecfef17490cebf80cca8b2393e97f9b69980
calebeLAR <ca6ele@gmail.com>
2023-09-21 01:35:38 -0400

    req08: implementa search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,21 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = list()
+    regex = re.compile(title.lower(), re.IGNORECASE)
+    searched_news = search_news({"title": {"$regex": regex}})
+
+    if not searched_news:
+        return []
+
+    for new in searched_news:
+        result.append((new["title"], new["url"]))
+
+    return result
 
 
 # Requisito 8
